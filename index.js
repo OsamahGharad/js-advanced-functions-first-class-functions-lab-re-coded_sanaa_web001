9515876a8af101b067fd6254a11cffcb588e37ef
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-17 22:16:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers =function(drivers){
+  return array.slice(0,2);
+}
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(drivers.length-2);
+}
+const selectingDrivers =[returnFirstTwoDrivers(),returnLastTwoDrivers()];
+
+const createFareMultiplier=function(num){
+  return function(fare){
+    return fare * num ;
+  }
+  
+}
+const fareDoubler =finction (){
+  
+}
